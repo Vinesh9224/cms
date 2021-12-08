50a34ff82f5d60d9aa6dc5550be1f28bbebd77b9
Vinesh9224 <56472116+Vinesh9224@users.noreply.github.com>
2021-12-08 17:07:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-public class GFG {
-    public static void main(String[] args)
-    {
-        int radius;
-        double pi = 3.142, area;
-        radius = 5;
-        // calculating the area of the circle
-        area = pi * radius * radius;
-        // printing the area of the circle
-        System.out.println("Area of circle is :" + area);
+const { exec } = require("child_process");
+const {marks}=require('./services/marks.microservice')
+const {students}=require('./services/students.microservice')
+
+
+exec("node services/marks.microservice.js", (error, stdout, stderr) => {
+    marks.listen("3000", () => {
+        console.log("📗 :\u001b[1;32m Student microservice is running on port 3000");
+        console.log("Listening on 3000");
+    })
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
     }
-}
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+exec("node services/students.microservice.js", (error, stdout, stderr) => {
+    students.listen("3001", () => {
+        console.log("📗 :\u001b[1;32m Marks microservice is running on port 3001");
+        console.log("Listening on 3001");
+        
+      });
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})
